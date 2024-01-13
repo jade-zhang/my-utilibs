cc6bc9448d5ad2c01f7686143d15fcdeee1370f7
刘俊杰 <liujj1.bj@chinatelecom.cn>
2024-01-13 19:59:08 +0800

    feat: 初始化项目

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: liujj
+ * @Date: 2024-01-11 21:56:25
+ * @LastEditors: liujj
+ * @LastEditTime: 2024-01-11 22:20:07
+ * @Description: 
+ */
+import deepClone from "./deepClone";
+
+export {
+  deepClone
+};
